3e4f0f265d6f38240f8c608a0e64dca127fe6cdd
_klein_ <_352641075@qq.com_>
2024-05-30 14:20:40 +0800

    移除 cmake 的构建目录

--- cmake-build-debug/external/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/apple/Desktop/CGProject/external/glm/glm
-# Build directory: /Users/apple/Desktop/CGProject/cmake-build-debug/external/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
